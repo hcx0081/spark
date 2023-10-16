f072604ea8fcfa489f4d6b5ef4bd94286b927226
hcx0081 <623363564@qq.com>
2023-10-16 21:52:37 +0800

    spark

--- rdd/src/main/scala/com/spark/operation/transformation/pair/CombineByKeyOp.scala
+++ rdd/src/main/scala/com/spark/operation/transformation/pair/CombineByKeyOp.scala
@@ -3,17 +3,20 @@ package com.spark.operation.transformation.pair
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-object AggregateByKeyOp {
+object CombineByKeyOp {
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("AggregateByKeyOp")
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("CombineByKeyOp")
     val sc = new SparkContext(sparkConf)
 
     val rdd: RDD[(String, Int)] = sc.makeRDD(List(
       ("a", 1), ("a", 2), ("b", 1), ("b", 2), ("b", 3)
     ), 2)
 
-    val aggregateByKeyRdd: RDD[(String, Int)] = rdd.aggregateByKey(100)(
-      (x, y) => { // x始终是100
+    val combineByKeyRdd: RDD[(String, Int)] = rdd.combineByKey(
+      v => {
+        v * 100
+      },
+      (x, y) => {
         Math.max(x, y)
       },
       (x, y) => {
@@ -21,7 +24,7 @@ object AggregateByKeyOp {
       }
     )
 
-    aggregateByKeyRdd.collect().foreach(println)
+    combineByKeyRdd.collect().foreach(println)
     // (b,100)
     // (a,100)
 
--- rdd/src/main/scala/com/spark/operation/transformation/pair/ReduceByKeyOp.scala
+++ rdd/src/main/scala/com/spark/operation/transformation/pair/ReduceByKeyOp.scala
@@ -7,17 +7,17 @@ object ReduceByKeyOp {
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("ReduceByKeyOp")
     val sc = new SparkContext(sparkConf)
-    
+
     val rdd: RDD[(String, Int)] = sc.makeRDD(List(
-      ("a", 1), ("a", 1), ("b", 1), ("b", 1), ("b", 1)
+      ("a", 1), ("a", 2), ("b", 1), ("b", 2), ("b", 3)
     ), 2)
-    
+
     val reduceByKeyRdd: RDD[(String, Int)] = rdd.reduceByKey(_ + _)
-    
+
     reduceByKeyRdd.foreach(println)
-    // (a,2)
-    // (b,3)
-    
+    // (b,6)
+    // (a,3)
+
     sc.stop()
   }
 }
