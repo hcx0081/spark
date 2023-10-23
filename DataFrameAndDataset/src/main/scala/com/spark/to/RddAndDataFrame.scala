b73ef402bfef6cd6ec65ba4853fed884a5fc6fed
hcx0081 <623363564@qq.com>
2023-10-23 20:16:05 +0800

    spark

--- DataFrameAndDataset/src/main/scala/com/spark/to/RddAndDataFrame.scala
+++ DataFrameAndDataset/src/main/scala/com/spark/to/RddAndDataFrame.scala
@@ -1,18 +1,20 @@
-package com.spark
+package com.spark.to
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 
-object ToMain {
+object RddAndDataFrame {
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("ToMain")
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("RddAndDataFrame")
     val sc = new SparkContext(sparkConf)
-    val spark = SparkSession.builder().config(sparkConf).getOrCreate() // 引入隐式转换规则
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    // import spark.implicits._ // 引入隐式转换规则
     
     /* RDD => DataFrame */
-    // val rdd: RDD[Int] = sc.makeRDD(Seq(("zs", 20), ("ls", 20), ("ww", 20)))
+    // val rdd: RDD[(String, Int)] = sc.makeRDD(Seq(("zs", 20), ("ls", 20), ("ww", 20)))
     // val df: DataFrame = rdd.toDF()
+    // val df: DataFrame = spark.createDataFrame(rdd)
     //
     // df.show()
     
---
+++ DataFrameAndDataset/src/main/scala/com/spark/to/RddAndDataset.scala
@@ -0,0 +1,27 @@
+package com.spark.to
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object RddAndDataset {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("RddAndDataset")
+    val sc = new SparkContext(sparkConf)
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    import spark.implicits._ // 引入隐式转换规则
+    
+    /* RDD => Dataset */
+    // val rdd: RDD[(String, Int)] = sc.makeRDD(Seq(("zs", 20), ("ls", 20), ("ww", 20)))
+    // val ds: Dataset[(String, Int)] = rdd.toDS()
+    // // val ds: Dataset[(String, Int)] = spark.createDataset(rdd)
+    //
+    // ds.show()
+    
+    /* Dataset => RDD */
+    val ds: Dataset[(String, Int)] = spark.createDataset(Seq(("zs", 20), ("ls", 20), ("ww", 20)))
+    val rdd: RDD[(String, Int)] = ds.rdd
+    
+    rdd.collect().foreach(println)
+  }
+}
